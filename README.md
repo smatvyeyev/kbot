64298064104dded3e3521bd3d44a37870066c4ca
smatvyeyev <30805877+smatvyeyev@users.noreply.github.com>
2023-05-24 23:02:27 +0000

    First build

--- README.md
+++ README.md
@@ -1,3 +1,12 @@
 # kbot
 app from scratch
 kbot app complete version 1.0.0
+
+gofmt -s -w ./
+@smatvyeyev ➜ /workspaces/kbot (main) $ go get
+@smatvyeyev ➜ /workspaces/kbot (main) $ go build -ldflags "-X="github.com/smatvyeyev/kbot/cmd.appVersion=v1.0.1
+@smatvyeyev ➜ /workspaces/kbot (main) $ ./kbot start
+kbot called v1.0.1 started2023/05/24 23:00:21 Please check TELE_TOKEN variable. %stelegram: Not Found (404)
+
+@smatvyeyev ➜ /workspaces/kbot (main) $ ./kbot version
+v1.0.1
--- cmd/kbot.go
+++ cmd/kbot.go
@@ -20,11 +20,11 @@ var (
 
 // kbotCmd represents the kbot command
 var kbotCmd = &cobra.Command{
-	Use:   "kbot",
-	Short: "A brief description of your command",
+	Use:     "kbot",
+	Aliases: []string{"start"},
+	Short:   "A brief description of your command",
 	Long: `A longer description that spans multiple lines and likely contains examples
 and usage of using your command. For example:
-
 Cobra is a CLI library for Go that empowers applications.
 This application is a tool to generate the needed files
 to quickly create a Cobra application.`,
@@ -39,7 +39,7 @@ to quickly create a Cobra application.`,
 			log.Fatal("Please check TELE_TOKEN variable. %s", err)
 			return
 		}
-		kbot.Handle(telebot.OnText, func(m tele.Context) error {
+		kbot.Handle(telebot.OnText, func(m telebot.Context) error {
 			log.Print(m.Message().Payload, m.Text())
 			return err
 		})
